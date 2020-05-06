c25f2b4c4623a7d421a9a438d7f21db0eafec512
Jewell <jhuffman815@gmail.com>
2020-05-06 03:20:09 -0400

    Axios/Fetch data

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Heyyy</h1>
-    </div>
-  );
+import { Provider } from "react-redux";
+import store from "./store";
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
-
 export default App;
