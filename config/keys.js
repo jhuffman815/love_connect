a9d23669a7aaefe2e93b16dc1e0c64ba5b57e7a9
Jewell <jhuffman815@gmail.com>
2020-05-07 23:02:45 -0400

    Reconfigure backend

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://jhuffman815:Perid0tt%210515@users-loeph.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: "secret"
-};
-
--- server.js
+++ server.js
@@ -1,39 +1,26 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-
-const users = require("./routes/api/users");
-
+require('rootpath')();
+const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
 
-//Bodyparser middleware
-app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
-);
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
-//DB Config
-const db = require("./config/keys").mongoURI;
+// use JWT auth to secure the api
+app.use(jwt());
 
-//Connect to MongoDB
-mongoose
-    .connect (
-        db,
-        { useNewUrlParser: true }
-    )
-    .then(() => console.log("MongoDB successfully connected"))
-    .catch(err => console.log(err));
+// api routes
+app.use('/users', require('./users/users.controller'));
 
-// Passport middleware
-app.use(passport.initialize());
-// Passport config
-require("./config/passport")(passport);
-// Routes
-app.use("/api/users", users);
+// global error handler
+app.use(errorHandler);
 
-const port = process.env.PORT || 5000; //to deploy to Heroku pointerEvents: 
-
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
